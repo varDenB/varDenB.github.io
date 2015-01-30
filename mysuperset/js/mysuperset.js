0936b6f2797616bdb547365502e28da411cdf7bc
varDenB <denisbiletskiy@gmail.com>
2015-01-30 16:51:52 +0200

    211

--- mysuperset/js/mysuperset.js
+++ mysuperset/js/mysuperset.js
@@ -1,3 +1,48 @@
 $( document ).ready(function() {
-   
+	var prepareTime=10000;
+   var buttonStart = document.querySelector('.btn,.btn-start');
+   var buttonPause = document.querySelector('.btn,.btn-pause');
+   var timer=document.querySelector('.timer-current span');
+   var intervalId=null;
+   var elapsedTime=0;
+   var a;
+   buttonStart.addEventListener('click',startsuperset,false);
+
+   function startsuperset(){
+
+   countdown(prepareTime);
+	if(a===true){
+		countdown(50000);
+	}
+
+   }
+   function countdown(time){
+		var startTime = (new Date()).getTime()+time;
+			intervalId = setInterval(function() {
+				var ticTime = (new Date()).getTime();
+				elapsedTime = (startTime-ticTime);
+				drawTime(elapsedTime);
+				console.log(elapsedTime);
+				if(elapsedTime<=0) {
+				clearInterval(intervalId);
+				intervalId=null;
+				a=true;
+				}
+			}, 100);
+			
+   }
+
+   function drawTime(elapsedTime) {
+   			var sec_num = parseInt(elapsedTime, 10);
+		   	var minutes = Math.floor(parseInt(elapsedTime / 60000)) % 60;
+			if (minutes < 1) minutes = 0;
+			sec_num = parseInt(elapsedTime - minutes * 60000);
+			if (minutes < 10) minutes = '0' + minutes;
+         	var seconds =  Math.floor(parseInt(elapsedTime / 1000)) % 60;
+			if (seconds < 1) seconds = 0;
+			sec_num = parseInt(elapsedTime - seconds * 1000);
+			if (seconds < 10) seconds = '0' + seconds;
+			var time = minutes + ':' + seconds;
+			timer.innerHTML = time;
+	}
 });
