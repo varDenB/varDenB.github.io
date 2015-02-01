004f916c96d505305a3f9e14a7afb48481f25e11
varDenB <denisbiletskiy@gmail.com>
2015-02-01 13:12:39 +0200

    add wav

--- mysuperset/js/mysuperset.js
+++ mysuperset/js/mysuperset.js
@@ -1,27 +1,43 @@
 $( document ).ready(function() {
 	var prepareTime=10000;
 	var doTime=45000;
+	var restTime=15000;
+	var setRestTime=60000;
    var buttonStart = document.querySelector('.btn,.btn-start');
    var buttonPause = document.querySelector('.btn,.btn-pause');
    var timer=document.querySelector('.timer-current span');
    var intervalId=null;
    var elapsedTime=0;
-   buttonStart.addEventListener('click',startsuperset,false);
-
-   function startsuperset(){
-	var startTime = (new Date()).getTime()+prepareTime;
+   buttonStart.addEventListener('click',startSuperSet,false);
+ 
+   function startSuperSet(){
+   	var startTime = (new Date()).getTime()+prepareTime;
+	var status='prepareTime';
+	var counter=0;
 			intervalId = setInterval(function() {
 				var ticTime = (new Date()).getTime();
 				elapsedTime = (startTime-ticTime);
 				drawTime(elapsedTime);
 				console.log(elapsedTime);
 				if(elapsedTime<=0) {
-				startTime = (new Date()).getTime()+doTime;
+				document.getElementById('sound1').play();
+				if(status==='prepareTime'||status==='restTime'&&counter!==3){
+					startTime = (new Date()).getTime()+doTime;
+					status='doTime';
+					counter+=1;
+				}else if(status==='doTime'&&counter!==3){
+					startTime = (new Date()).getTime()+restTime;
+					status='restTime';
+				}else{
+					startTime = (new Date()).getTime()+setRestTime;
+					status='restTime';
+					counter=0;
+				}
 				}
 			}, 100);
 
    }
-  
+
    function drawTime(elapsedTime) {
    			var sec_num = parseInt(elapsedTime, 10);
 		   	var minutes = Math.floor(parseInt(elapsedTime / 60000)) % 60;
@@ -36,3 +52,4 @@ $( document ).ready(function() {
 			timer.innerHTML = time;
 	}
 });
+
