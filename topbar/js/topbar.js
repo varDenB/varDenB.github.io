4fda528b6c2eb82ba6214c27a20b12781ccfc04c
varDenB <denisbiletskiy@gmail.com>
2015-04-14 17:01:16 +0300

    10

--- topbar/js/topbar.js
+++ topbar/js/topbar.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
+	
+	var $topbarhelp = $(".topbar-help");
+
 	$(".topbar .glyphicon-remove-circle").click(function() {
 		$(".topbar").css("display", "none");
+	});
+
+	$(".topbar .glyphicon-question-sign").click(function() {
+		if ($topbarhelp.css('display') === 'none') {
+			$topbarhelp.slideDown("slow");
+		} else {
+			$topbarhelp.slideUp('slow');
+		}
 	});
 
 	setTimeout(function() {
 		$('.topbar-search').css("display", "none");
 		$('.topbar-answer').css("display", "block");
-	}, 4000);
+	}, 5000);
+
 });
