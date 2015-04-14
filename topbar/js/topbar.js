5a0508c1fe34922b801ab897262b1f36066d7fa8
varDenB <denisbiletskiy@gmail.com>
2015-04-14 14:40:01 +0300

    7

--- topbar/js/topbar.js
+++ topbar/js/topbar.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-	$(".topbar .glyphicon-remove-circle").click(function () {
-      $(".topbar").css("display","none");
-    });
+	$(".topbar .glyphicon-remove-circle").click(function() {
+		$(".topbar").css("display", "none");
+	});
+
+	setTimeout(function() {
+		$('.topbar-search').css("display", "none");
+		$('.topbar-answer').css("display", "block");
+	}, 4000);
 });
