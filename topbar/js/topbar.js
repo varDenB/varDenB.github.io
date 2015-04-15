d40ce2b7aaadea67993783590c546dda01057e15
varDenB <denisbiletskiy@gmail.com>
2015-04-15 14:35:41 +0300

    12

--- topbar/js/topbar.js
+++ topbar/js/topbar.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
 	
 	var $topbarhelp = $(".topbar-help");
+	var $questionSign=$('.glyphicon-question-sign');
+	var $hOffer=$('.topbar-h-offer');
+	var $detailOffer=$('.topbar-detail-offer');
+	var $hCompetitors=$('.topbar-h-competitors');
+	var $detailCompetitors=$('.topbar-detail-competitors');
+	var $controls=$('.topbar-controls');
+	var $popovers=[$hOffer,$detailOffer,$hCompetitors,$detailCompetitors,$controls];
 
 	$(".topbar .glyphicon-remove-circle").click(function() {
 		$(".topbar").css("display", "none");
 	});
+	$('.glyphicon-question-sign').popover('show');
 
 	$(".topbar .glyphicon-question-sign").click(function() {
-		if ($topbarhelp.css('display') === 'none') {
-			$topbarhelp.slideDown("slow");
-		} else {
-			$topbarhelp.slideUp('slow');
+
+		$('.topbar .blur').css('display','block');
+		$('.glyphicon-question-sign').popover('hide');
+
+		
+		setInterval(function() {
+			for(var i=0;i<$popovers.length;i+=1){
+			$popovers[i].popover('show');
 		}
+		},2000);	
+		
+		
 	});
 
 	setTimeout(function() {
 		$('.topbar-search').css("display", "none");
 		$('.topbar-answer').css("display", "block");
 	}, 5000);
-
 });
